6ef5c843d6042398b4e795c6d057ff08f9324d5a
Navya <51898740+Navya-Kota@users.noreply.github.com>
2020-02-20 23:20:33 +0530

    Home Task 3

---
+++ Hometask03/App.java
@@ -0,0 +1,13 @@
+package CMRIT.Navya.Hometask03;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Hometask03/Client.java
@@ -0,0 +1,10 @@
+package CMRIT.Navya.Hometask03;
+
+public class Client extends InterestCalculator{
+		public static void main(String args[]) {
+			InterestCalculator i = new InterestCalculator();
+			i.Interest();
+		}
+
+	}
+
